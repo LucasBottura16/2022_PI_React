d614c6249e042d59e64abf54de5090a96ab2f180
LucasBottura16 <bdfbottura@gmail.com>
2022-11-03 15:39:01 -0300

    commit test Cria

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App do PI Novo!</Text>
+      <Text>App do PI cria!</Text>
       <StatusBar style="auto" />
     </View>
   );
